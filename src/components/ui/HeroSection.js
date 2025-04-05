7269f781af1d37431d945bbdd8f426dfc3ec3f11
Samar Siddiqui <samarsiddiqui3010@gmail.com>
2025-04-06 02:12:01 +0530

    Added Explore Animation on Hero Section

--- src/components/ui/HeroSection.js
+++ src/components/ui/HeroSection.js
@@ -1,9 +1,28 @@
 "use client";
 
-import React from "react";
+import React, { useRef } from "react";
 import Image from "next/image";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 const HeroSection = () => {
+
+  const dotsRef = useRef([])
+
+  useGSAP(() => {
+    gsap.fromTo(
+      dotsRef.current,
+      { y: 0 },
+      {
+        y: -10,
+        repeat: -1,
+        yoyo: true,
+        stagger: 0.5,
+        ease: "power1.inOut",
+        duration: 0.6,
+      }
+    );
+  }, []);
   return (
     <>
         <video
@@ -22,7 +41,25 @@ const HeroSection = () => {
           loop
           playsInline
           src='/assets/videos/gulz wide.mp4'
+
+          
         ></video>
+
+<div className="absolute bottom-2 left-1/2 transform -translate-x-1/2 flex flex-col items-center text-white">
+  <h1 className="uppercase text-lg font-gothic tracking-wide mb-3 cursor-pointer relative group ">
+  Explore
+  <span className="absolute bottom-0.5 left-0 h-[1px] w-0 bg-white transition-all duration-300 group-hover:w-full"></span>
+</h1>
+    <div className="flex flex-col items-center gap-2">
+          {[0, 1, 2].map((_, index) => (
+            <span
+              key={index}
+              ref={(el) => (dotsRef.current[index] = el)}
+              className="w-1.5 h-1.5 rounded-full bg-white"
+            ></span>
+          ))}
+        </div>
+  </div>
       </>
   );
 };
